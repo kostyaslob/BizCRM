7fe07d02064f43600b027526d1c8d1eda974627a
kostyaslob <kostyaslob@gmail.com>
2025-10-14 21:47:18 +0300

    add Button in Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { headers } from "next/headers";
 import StatusLabel, { Status } from "./components/status-label"
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()} </h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
