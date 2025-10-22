51dff434c42dd651f32328dd6d1bfcc82f30fb28
kostyaslob <kostyaslob@gmail.com>
2025-10-22 21:12:28 +0300

    add fetch requests

--- src/app/(admin)/dashboard/@promotions/page.tsx
+++ src/app/(admin)/dashboard/@promotions/page.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { getSummaryPromotions } from "@/lib/api";
-import SummaryTable from "@/app/components/summary-table";
-import SummaryTableHeader from "@/app/components/summary-table-header";
-import SummaryTableCell from "@/app/components/summary-table-cell";
-import DashboardCard from "@/app/components/dashboard-card";
+import React from 'react';
+import { getPromotions } from '@/lib/api';
+import SummaryTable from '@/app/components/summary-table';
+import SummaryTableHeader from '@/app/components/summary-table-header';
+import SummaryTableCell from '@/app/components/summary-table-cell';
+import DashboardCard from '@/app/components/dashboard-card';
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryPromotions();
+  const data = await getPromotions();
 
   return (
     <DashboardCard label="Promotions">
@@ -21,10 +21,10 @@ export default async function Page({}: PageProps) {
           </>
         }
       >
-        {data.map(({ promotionId, promotionName, companyTitle, discount }) => (
-          <tr key={promotionId}>
+        {data.map(({ id, title, companyTitle, discount }) => (
+          <tr key={id}>
             <SummaryTableCell>{companyTitle}</SummaryTableCell>
-            <SummaryTableCell>{promotionName}</SummaryTableCell>
+            <SummaryTableCell>{title}</SummaryTableCell>
             <SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
           </tr>
         ))}
