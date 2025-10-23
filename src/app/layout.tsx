ff9fd9f6702c47a122b1c6722fc173a2448988cd
kostyaslob <kostyaslob@gmail.com>
2025-10-23 16:55:24 +0300

    update some components and routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import Providers from "./components/providers";
 
-const font = Plus_Jakarta_Sans({subsets: ["latin"]});
+const font = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -11,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
