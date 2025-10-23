aa86c4fb8faadaaba8e601b6b1606019f77151f6
kostyaslob <kostyaslob@gmail.com>
2025-10-23 17:13:58 +0300

    add PromotionForm

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -105,31 +105,31 @@ export const getPromotions = async (
   );
 };
 
-// export const createCompany = async (
-//   data: Omit<Company, 'id' | 'hasPromotions'>,
-//   init?: RequestInit,
-// ) => {
-//   return sendRequest<Company>(buildUrl('companies'), {
-//     ...init,
-//     method: 'POST',
-//     body: JSON.stringify(data),
-//     headers: {
-//       ...(init && init.headers),
-//       'content-type': 'application/json',
-//     },
-//   });
-// };
+export const createCompany = async (
+  data: Omit<Company, 'id' | 'hasPromotions'>,
+  init?: RequestInit,
+) => {
+  return sendRequest<Company>(buildUrl('companies'), {
+    ...init,
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      ...(init && init.headers),
+      'content-type': 'application/json',
+    },
+  });
+};
 
-// export const createPromotion = async (
-//   data: Omit<Promotion, 'id'>,
-//   init?: RequestInit,
-// ) => {
-//   return sendRequest<Promotion>(buildUrl('promotions'), {
-//     method: 'POST',
-//     body: JSON.stringify(data),
-//     headers: {
-//       ...(init && init.headers),
-//       'content-type': 'application/json',
-//     },
-//   });
-// };
+export const createPromotion = async (
+  data: Omit<Promotion, 'id'>,
+  init?: RequestInit,
+) => {
+  return sendRequest<Promotion>(buildUrl('promotions'), {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      ...(init && init.headers),
+      'content-type': 'application/json',
+    },
+  });
+};
